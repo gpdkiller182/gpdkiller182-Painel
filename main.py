ce0c33343b14fb52ad6583ee62b31ea06b631238
gpdkiller182 <83891308+gpdkiller182@users.noreply.github.com>
2021-06-25 12:44:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 global R,B,C,Y,G,RT,CY,CO
 CO='\033[m';R='\033[1;31m';B='\033[1;34m';C='\033[1;37m';CY='\033[1;36m';Y='\033[1;33m';G='\033[1;32m';RT='\033[;0m';NO_FORMAT="\033[0m";C_GREY89="\033[38;5;254m";C_RED1="\033[48;5;196m"
 #######################
-## RECODADO POR YATO ##
+## RECODADO POR GØD KILLER ##
 #######################
 
 def restart():
@@ -33,7 +33,7 @@ except Exception as error:
 def clear():
 	os.system('cls');os.system('clear')
 
-requests = requests.Session();result = os.popen('figlet KINY').read()
+requests = requests.Session();result = os.popen('figlet GØD KILLER').read()
 
 try:
     if __name__ == '__main__':
